31c14c29bb5efcae1a879cdbf75147ec1c0f2368
Daniel <jodsalazarmo@unal.edu.co>
2015-03-26 20:45:28 -0500

    mas nombres y membresía

--- src/QuickSortJava.java
+++ src/QuickSortJava.java
@@ -1,3 +1,12 @@
+/**
+ * Created by Kar-garoo:
+
+ *Jose Daniel Salazar Mora
+ *Sergio Alejandro Diaz Pinilla
+ *Julian Camilo Mendez Corredor
+ *Miguel Andres Torres Chavarro
+ */
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
