6a600138c030442e609bb5233e0d2f09e1f2089e
Daniel <jodsalazarmo@unal.edu.co>
2015-03-26 20:34:46 -0500

    condición <

--- src/BubbleSort.groovy
+++ src/BubbleSort.groovy
@@ -11,7 +11,7 @@ class BubbleSort extends SortMethod {
 
     def sortM(a) {
         report.instanceSize = a.size()
-        if(a.size()<= 1) {
+        if(a.size()< 2) {
             return a
         }
         for(out in (a.size()-2)..0)
